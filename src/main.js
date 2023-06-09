00286105a54dd86e246f17eba0752fdbb1d81b01
LilYuan <2507567008@qq.com>
2023-06-09 22:35:26 +0800

    home-图片懒加载指令优化

--- src/main.js
+++ src/main.js
@@ -9,12 +9,13 @@ import router from './router'
 import '@/styles/common.scss'
 
 
-import { useIntersectionObserver } from '@vueuse/core'
-// //测试接口函数
+
 // import {getCategoryAPI} from '@/apis/testAPI'
 // getCategoryAPI().then(res=>{
 //     console.log(res)
 // })
+
+import { lazyPlugin } from '@/directives'
 
 
 const app = createApp(App)
@@ -22,23 +23,27 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
+app.use(lazyPlugin)
+
 app.mount('#app')
 
-app.directive('img-lazy', {
-    mounted (el, binding) {
-      // el: 指令绑定的那个元素 img
-      // binding: binding.value  指令等于号后面绑定的表达式的值  图片url
-      console.log(el, binding.value)
-      const { stop } = useIntersectionObserver(
-        el,
-        ([{ isIntersecting }]) => {
-          console.log(isIntersecting)
-          if (isIntersecting) {
-            // 进入视口区域
-            el.src = binding.value
-            stop()
-          }
-        },
-      )
-    }
-  })
+
+//定义全局指令（不推荐） 入口文件只做初始化 应该定义一个插件 然后导入 注册 就行
+// app.directive('img-lazy', {
+//     mounted (el, binding) {
+//       // el: 指令绑定的那个元素 img
+//       // binding: binding.value  指令等于号后面绑定的表达式的值  图片url
+//       console.log(el, binding.value)
+//       const { stop } = useIntersectionObserver(
+//         el,
+//         ([{ isIntersecting }]) => {
+//           console.log(isIntersecting)
+//           if (isIntersecting) {
+//             // 进入视口区域
+//             el.src = binding.value
+//             stop()
+//           }
+//         },
+//       )
+//     }
+//   })
