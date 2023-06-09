c657ffa9da835b5010e1b86c67dad5ab002ff40f
LilYuan <2507567008@qq.com>
2023-06-09 18:57:03 +0800

    home-面板组件的封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import homeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -14,4 +15,15 @@ import homeProduct from './components/HomeProduct.vue'
   <HomeNew />
   <HomeHot />
   <homeProduct />
+  <!-- 测试面板组件 -->
+  <HomePanel title="新鲜好物" sub-title="新鲜好物 好多商品">
+<div>
+    我是新鲜好物的插槽
+</div>
+  </HomePanel>
+  <HomePanel title="人气商品" sub-title="人气商品 好多商品">
+<div>
+    我是人气商品的插槽
+</div>
+  </HomePanel>
 </template>
